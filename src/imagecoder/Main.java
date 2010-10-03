cca4ddb7a5d0659d1e753c2fa90f659d21b0b58f
Barend Scholtus <barend.scholtus@gmail.com>
2010-10-03 15:32:17 +0700

    Added the Apache License 2.0 to original source files.
    Added LICENSE and NOTICE.

--- src/imagecoder/Main.java
+++ src/imagecoder/Main.java
@@ -1,13 +1,27 @@
 /*
- * Main.java
+ *  Copyright 2010 Barend Scholtus
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 package imagecoder;
 
-import java.awt.DisplayMode;
-import java.awt.GraphicsEnvironment;
+import java.awt.*;
 import javax.swing.*;
 
 /**
+ * Start-up code for the Image Coder program.
+ * It switches Swing to the System Look And Feel, and creates a controller frame
+ * and image viewer frame.
  *
  * @author Barend Scholtus
  */
--- src/util/MasterJFrame.java
+++ src/util/MasterJFrame.java
@@ -1,12 +1,21 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ *  Copyright 2010 Barend Scholtus
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
-
 package util;
 
-import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
+import java.awt.event.*;
 import java.util.ArrayList;
 import javax.swing.JFrame;
 
@@ -94,7 +103,7 @@ public class MasterJFrame extends JFrame implements WindowListener {
         }
 
         // activate each slave frame that isn't iconified
-        for (JFrame slave: slaveFrames)  {
+        for (JFrame slave : slaveFrames) {
             int state = slave.getExtendedState();
             if ((state & JFrame.ICONIFIED) == 0) {
                 slave.toFront();
